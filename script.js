279c51d404df2593e6c0b62c7efdddf6bd8ad23a
Aexeron <tchouawoutimondaniel@gmail.com>
2024-11-01 11:02:07 +0300

    finish game.

--- script.js
+++ script.js
@@ -2,35 +2,76 @@
 
 let computeresult = 0
 let humanchoice = 0
+let winner = 0
 
 function computerchoice (randint) {
-   if ( Math.random < 0.300 ) {
-    computeresult = "rock";
-    
-   }
+    randint = Math.random()
 
-   if ( Math.random < 0.600 && Math.random > 0.300) {
-    computeresult = "scissors";
-    
-   }
+    if (randint <= 0.300) {
+        randint="rock";
+        computeresult= randint
+        return(computeresult)
+    }
 
-   if ( Math.random > 0.600) {
-    computeresult ="paper";
-    
-   }
-   //this function helps find the outcome of the computer
-   return(computeresult)
+    if (randint <= 0.600 && randint > 300) {
+        randint="paper";
+        computeresult = randint
+        return(computeresult)
+    }
+
+    if (randint  > 0.600) {
+        randint="scissors";
+        computeresult = randint
+        return(computeresult)
+    }
 }
 
 function gethumanchoice (choice) {
     
-    if ( humanchoice == "rock", "paper", "scissors") {
-        return (humanchoice)
+    if ( humanchoice == "1", "2", "3") {
+        if (humanchoice == "1") {
+            humanchoice = "rock"
+        }
+
+        if (humanchoice == "2") {
+            humanchoice = "paper"
+        }
+
+        if (humanchoice == "3") {
+            humanchoice = "scissors"
+        }
+    
     }
 }
 
+
+
+function findwinner (rice){
+if (computeresult == humanchoice) {
+    return("it's a tie")
+} else if (computeresult == "scissors" && humanchoice == "paper" || computeresult == "rock" && humanchoice == "scissors" || computeresult == "paper" && humanchoice == "rock" ) {
+    return("computer won")
+} else if (humanchoice == "scissors" && computeresult == "paper" || humanchoice == "rock" && computeresult == "scissors" || humanchoice == "paper" && computeresult == "rock" ) {
+    return "you won"
+}
+   
+    } 
+
+
+
+    
+
+   
+    
+   
+
+
+
+  
 humanchoice = parseInt(prompt("Enter either Rock, Paper or Scissors. Rock= 1 Paper= 2 Scissors= 3"))
 gethumanchoice(0)
-computerchoice(0)
+
 console.log(humanchoice)
-console.log(computeresult)
+console.log(computerchoice(0))
+console.log(findwinner(0))
+
